396a4938d31bb01d2004264c87d97562aa7549b3
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-11-08 18:47:45 +0530

    commit 1 on print.py

--- print.py
+++ print.py
@@ -1,1 +1,1 @@
-print("My name is " + input("what is your name") + " and I am " + str(input("What is your age")) + "year old")
+print("My name is " + input("what is your name") + " and I am " + str(input("What is your age")) + " year old")
