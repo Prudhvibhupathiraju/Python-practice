cea2d84c20aa6524d5a4f83494cdb62dccd2310a
EC2 Default User <ec2-user@ip-172-31-15-117.ap-south-1.compute.internal>
2025-11-09 08:19:19 +0000

    updating ridebill.py

--- ridebill.py
+++ ridebill.py
@@ -1,10 +1,21 @@
 height = int(input("What is your height in cm?\n"))
 if height > 120:
     age = int(input("what is your age?\n"))
-    if age <= 18:
+    bill = 0
+    if age < 12:
+        print("Pay $5")
+        bill = 5
+    elif age >= 12 and age <= 18:
+        bill = 7
         print("Pay $7")
     else:
+        bill = 12
         print("Pay $12")
+    pic = input("Do you want to take a pic? Y or N")
+    if pic == "Y":
+         bill += 3
+    print(f"The bill amount is {bill}")
+
 else:
     print("You canot ride")
 
