e507de887b2e06330f66a8d260404c8896cb02fc
unknown <prudhvirajubh999@gmail.com>
2025-11-11 23:05:10 +0530

    adding new files to repo

---
+++ print.py
@@ -0,0 +1,31 @@
+# print("hello world\ni am prudhvi")
+# print("Hello" + " Ayaansh")
+# print("Hello " + input("type your name") + "!")
+# car_name = input("name of your car")
+# print(car_name)
+# print(len(car_name))
+# car_name = "venue"
+# print(car_name)
+# print("length of characters in your bike name is :" + str(len(input("tell me your bike name"))))
+
+# username = input("what is your username")
+# length_username = len(username)
+# print(length_username)
+
+# print("Welcome to band name generator")
+# city_name = input("What's the name of the city you grew in?\n")
+# pet_name = input("What's your pet name\n")
+# print("Your band name could be " + city_name + " " + pet_name)
+
+
+print("hello"[-2])
+print("123" + "3444")
+print(123+223)
+print(len("1238"))
+print(type("1238"))
+print(type(555))
+print(type(4445.25))
+print(type(True))
+
+print("Number of letters in your name is : " + str(len(input("what is your name\n"))))
+print("My name is " + input("what is your name\n") + " and my age is " + str(input("what is your age\n")))
