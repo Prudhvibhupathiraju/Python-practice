16bfb98e2f59833637cf265e83f1010376d2282e
unknown <prudhvirajubh999@gmail.com>
2025-11-14 02:16:56 +0530

    adding random states

---
+++ statesofindia.py
@@ -0,0 +1,41 @@
+import random
+
+states = [
+    "Odisha",
+    "Bihar",
+    "Assam",
+    "West Bengal",
+    "Uttar Pradesh",
+    "Madhya Pradesh",
+    "Andhra Pradesh",
+    "Rajasthan",
+    "Karnataka",
+    "Kerala",
+    "Tamil Nadu",
+    "Maharashtra",
+    "Gujarat",
+    "Nagaland",
+    "Haryana",
+    "Punjab",
+    "Himachal Pradesh",
+    "Manipur",
+    "Meghalaya",
+    "Tripura",
+    "Sikkim",
+    "Mizoram",
+    "Arunachal Pradesh",
+    "Goa",
+    "Chhattisgarh",
+    "Uttarakhand",
+    "Jharkhand",
+    "Telangana"
+]
+
+# print(len(states))
+print(states[random.randint(0, 27)])
+# states.append("Lakshadeep")
+# print(len(states))
+# print(states[-1])
+# states.extend(["aaa", "bbb"])
+# print(states[-1])
+# print(len(states))
