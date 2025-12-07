a6465a821330fc9d67753f72952834c3b6dd49ff
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-12-07 13:13:22 +0530

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,13 @@
+import random
+
+word_list = [ "aardvark", "baboon", "camel" ]
+word = random.choice(words)
+
+opt = int(input("Type a letter\n"))
+
+for letter in word:
+  if opt = letter:
+    print("Correct")
+  else:
+    print("Not Correct")
+  
