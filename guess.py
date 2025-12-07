172378cf27901dcddbbaa9b5a36365eb316eae66
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-12-07 13:30:14 +0530

    Update guess.py

--- guess.py
+++ guess.py
@@ -3,7 +3,7 @@ import random
 word_list = [ "aardvark", "baboon", "camel" ]
 word = random.choice(word_list)
 print(word)
-opt = str(input("Type a letter\n"))
+opt = str(input("Type a letter\n")).lower()
 
 for letter in word:
   if opt == letter:
