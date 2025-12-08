7eead1352d8c84270063b54b61e9b783bd58dded
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-12-08 13:22:47 +0530

    Update pointer.py

--- pointer.py
+++ pointer.py
@@ -1,7 +1,14 @@
 num1 = 11
 num2 = 11
 
-print("Before num2 value is updated/n")
+print("Before num2 value is updated")
+print(f"num1 = {num1}")
+print(f"num2 = {num2}")
+print("\nnum1 points to ", id(num1))
+print("num2 points to ", id(num2))
+num2 = 22
+
+print("After num2 value is updated")
 print(f"num1 = {num1}")
 print(f"num2 = {num2}")
 print("\nnum1 points to ", id(num1))
