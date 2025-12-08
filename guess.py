02e2d4fb389da0aad615f62f7f8fa49114e8881e
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-12-08 11:41:47 +0530

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,17 +1,20 @@
 import random
-
 word_list = [ "aardvark", "baboon", "camel" ]
 word = random.choice(word_list)
 print(word)
-
 display = []
-
 for i in word:
   display.append("_")
 print(" ".join(display))
-while ("".join(display)) != word:
+lives = 6
+while lives>0:
   opt = str(input("Type a letter\n")).lower()
   for index, letter in enumerate(word):
     if opt == letter:
-            display[index] = opt
+      display[index] = opt
+    elif opt != letter:
+      lives -= 1
+      if lives = 0:
+      print("Game Over!")
+    print(f"Your lives left:{lives}")  
   print(" ".join(display))
