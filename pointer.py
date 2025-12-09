c41b06f1c3211af191e81b6a1a6dcd8d6b7a726c
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-12-09 09:42:20 +0530

    Update pointer.py

--- pointer.py
+++ pointer.py
@@ -13,3 +13,12 @@ print(f"num1 = {num1}")
 print(f"num2 = {num2}")
 print("\nnum1 points to ", id(num1))
 print("num2 points to ", id(num2))
+
+dict1 = { 'value1' = 22 }
+dict2 = dict1
+
+print("Before dict2 value is updated")
+print(f"dict1 = {dict1}")
+print(f"dict2 = {dict2}")
+print("dict1 points to ", id(dict1))
+print("dict2 points to ", id(dict2))
