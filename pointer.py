df35e72a706993168184803fdbe7a310237bb864
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-12-09 10:01:38 +0530

    Update pointer.py

--- pointer.py
+++ pointer.py
@@ -30,3 +30,24 @@ print(f"dict1 = {dict1}")
 print(f"dict2 = {dict2}")
 print("dict1 points to ", id(dict1))
 print("dict2 points to ", id(dict2))
+
+dict3 = { 'value' : 33 }
+
+print("Before dict3 value is updated")
+print(f"dict1 = {dict1}")
+print(f"dict2 = {dict2}")
+print(f"dict3 = {dict3}")
+print("dict1 points to ", id(dict1))
+print("dict2 points to ", id(dict2))
+print("dict3 points to ", id(dict3))
+
+dict2 = dict3
+
+print("After dict3 value is updated")
+print(f"dict1 = {dict1}")
+print(f"dict2 = {dict2}")
+print(f"dict3 = {dict3}")
+print("dict1 points to ", id(dict1))
+print("dict2 points to ", id(dict2))
+print("dict3 points to ", id(dict3))
+
