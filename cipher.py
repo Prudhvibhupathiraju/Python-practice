b27e2c8f7cc72129e8db4e2a538c7a3ba3705bc8
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-12-10 12:15:22 +0530

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -8,13 +8,17 @@ shift = int(input("Type the shift number\n"))
 def encrypt(text, shift):
   encrypt_message = ""
   for letter in text.lower():
-    encrypt_message.append(alphabet[alphabet.index(letter) + shift])
-  print(join("",(encrypt_message)))
+    index = alphabet.index(letter)
+    new_index = (index + shift) % 26
+    encrypt_message += alphabet[new_index]  
+  print(encrypt_message)
 def decrypt(text, shift):
-  decrypt_message = []
+  decrypt_message = ""
   for letter in text.lower():
-    decrypt_message.append(alphabet[alphabet.index(letter) - shift])
-  print(join("",(decrypt_message)))
+    index = alphabet.index(letter)
+    new_index = (index - shift) % 26
+    decrypt_message += alphabet[new_index]  
+  print(encrypt_message)
 if direction == "encode":
   encrypt(text, shift)
 if direction == "decode":
