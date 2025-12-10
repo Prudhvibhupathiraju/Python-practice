040f2e73517910317ecb1f9488475ea2fb5d0ebf
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-12-10 09:32:58 +0530

    Update function.py

--- function.py
+++ function.py
@@ -19,5 +19,5 @@ def greet(name, location):
     print(f"Good Morning {name}, how is the weather today in {location}")
     print(f"How are you {name}")
 
-greet("Prudhvi", "Kaikaluru")
-greet("Babitha", "bhimavram")
+greet(location = "kaikaluru", name = "Prudhvi")
+greet(location = "Bhimavaram", name = "Babitha")
