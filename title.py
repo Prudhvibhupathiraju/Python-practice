64a41b1dea3778d70465a1ba140bd672f6e35920
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2025-12-13 10:27:49 +0530

    Update title.py

--- title.py
+++ title.py
@@ -1,7 +1,7 @@
 def format_name(f_name, l_name):
   formated_fname = f_name.title()
   formated_lname = l_name.title()
-  return(f"{formated_fname} {formatedd_lname}")
+  return(f"{formated_fname} {formated_lname}")
 f_name = str(input("Type first name\n"))
 l_name = str(input("Type last name\n"))
 print(format_name(f_name, l_name))
