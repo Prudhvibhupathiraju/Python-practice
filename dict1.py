507034edb9a41d0e605261cb7ce8f99532d4aed2
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2026-02-09 10:43:22 +0000

    Create dict1.py

---
+++ dict1.py
@@ -0,0 +1,17 @@
+dict1 ={
+  "username": "alpha_user92",
+  "session_id": "7f3k9x21",
+  "login_attempts": 4,
+  "is_active": True,
+  "account_balance": 1587.45,
+  "country": "India",
+  "preferred_language": "English",
+  "subscription_plan": "Premium",
+  "last_login": "2026-02-09T10:14:22",
+  "device_type": "Laptop"
+}
+
+print(dict1["is_active"])
+dict1["State"] = "Andhra Pradesh"
+print(dict1["State"])
+print(dict1)
